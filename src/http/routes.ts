2729b3647a6f8960d5ac0473faf0ed383057a5ff
Jennifer Takagi <jennifer.takagi@personio.de>
2024-03-24 12:47:03 +0100

    [chore] Set up test enviroment

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import { authenticate } from '@/http/controllers/authenticate'
 import { register } from './controllers/register'
 import { profile } from '@/http/controllers/profile'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
