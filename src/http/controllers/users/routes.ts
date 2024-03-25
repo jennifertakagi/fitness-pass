1d545ea0682d87666298afe442962acf45dad918
Jennifer Takagi <jennifer.takagi@personio.de>
2024-03-25 23:20:59 +0100

    [feat] Add RBAC

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,6 +2,8 @@ import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { register } from './register'
 import { refresh } from './refresh'
+import { FastifyInstance } from 'fastify'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
